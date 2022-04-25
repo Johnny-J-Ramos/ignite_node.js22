3e224897dd4914ff3f9babc028443ae22a9c14a5
Johnny Jefferson <johnnyjeffersonjr@hotmail.com>
2022-04-24 22:47:18 -0300

    iniciando o ignite

---
+++ fundamentos_node/src/index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express()
+const path = require('path')
+
+const alunos = []
+
+
+//Buscar uma informação dentro do servidor
+app.get('/curses', (req, res) => {
+    return res.json([
+        "Curse 01",
+        "Curse 02",
+        "Curse 03"
+    ]) 
+})
+
+//Inserir uma informação no servidor 
+app.post('/courses/id', (req, res) => {
+    return res.json([
+        "Curse 01",
+        "Curse 02",
+        "Curse 03",
+        "Curse 04"
+        
+    ]) 
+})
+
+
+//PUT Alterar uma informação no servidor
+app.put('/courses/id', (req, res) => {
+    return res.json([
+        "Curse 1",
+        "Curse 2",
+        "Curse 3",
+        "Curse 4"
+    ])
+})
+
+//PATCH Alterar uma informação especifíca 
+
+app.path('/courses/id', (req, res) => {
+    return res.json([
+        "Curse 11",
+        "Curse 2",
+        "Curse 3",
+        "Curse 4"
+    ])
+} )
+
+// DELETE Deletar uma inforamação no servidor
+
+app.delete('/courses/id', (req, res ) => {
+    return res.json([
+        "Curse 11",
+        "Curse 2",
+        "Curse 3"
+    ])
+})
+
+
+app.listen(3333)
