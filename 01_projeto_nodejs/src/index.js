1d6bc2f251ad1650a96af21e670133eda5299030
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2022-05-02 22:44:59 -0300

    continuando o primeiro projeto

--- 01_projeto_nodejs/src/index.js
+++ 01_projeto_nodejs/src/index.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
+const { v4: uuidn4 } = require('uuid')
 
+app.use(express.json());
+/**
+ * CPF - STRING
+ * NAME - STRING
+ * ID - UUID
+ * STATEMENT []
+ */
 
+const customers = [];
 
+//criação de uma conta
+app.post("/account", (req, res) => {
+   const {cpf, name} = Request.body;
+   const id = uuid.v4();
+   customers.push({
+      cpf,
+      name,
+      id,
+      statement: []
+   })
+  
+   return res.status(201).send()
+})
 
 
 
