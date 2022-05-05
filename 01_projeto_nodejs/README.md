043f6932dbeb9d4a885def7ea0e9a1fcd6075735
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2022-05-04 22:44:53 -0300

    criando os meus middlewares

--- 01_projeto_nodejs/README.md
+++ 01_projeto_nodejs/README.md
@@ -3,8 +3,8 @@
 
 ### Requesitos
 
-- [] Deve ser possível criar uma conta
-- [] Deve ser possível buscar o extrato bancário do cliente
+- [x] Deve ser possível criar uma conta
+- [x] Deve ser possível buscar o extrato bancário do cliente
 - [] Deve ser possível realizar um depósito
 - [] Deve ser possível buscar o extrato bancário do cliente por data
 - [] Deve ser possível atualizar dados da conta do cliente
@@ -15,7 +15,8 @@
 
 ## Regras de negócio
 
-- [] Não deve ser possível cadastrar uma conta com CPF já existente
+- [x] Não deve ser possível cadastrar uma conta com CPF já existente
+- [x] Não deve ser possível buscar extrato em uma conta não existente
 - [] Não deve ser possível fazer depósito em uma conta não existente
 - [] Não deve ser possível buscar extrato em uma conta não existente
 - [] Não deve ser possível fazer saque em uma conta existente
--- 01_projeto_nodejs/src/index.js
+++ 01_projeto_nodejs/src/index.js
@@ -1,24 +1,56 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
 const app = express();
-const { v4: uuidv4 } = require('uuid')
 
-
-
-
-/**
- * CPF - STRING
- * NAME - STRING
- * ID - UUID
- * STATEMENT []
- */
+app.use(express.json());
 
 const customers = [];
 
-//criação de uma conta
-app.post("/account", (req, res) => {
-   return res.json({})
-})
+
+//Middlewares
+function verifyExistsAccountCPF(req, res, next) {
+   const { cpf } = request.params;
+
+  customer = customers.find((customer) => customer.cpf === cpf)
+
+  if(!customer) {
+   return response.status(400).joson({error:"Customer not found"})
+}
+   return next()
+}
+
+//Criando uma conta com um único cpf
+app.post('/account', (request, response) => {
+  const { cpf, name } = request.body;
+
+  const customerAlreadyExistis = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if(customerAlreadyExistis) {
+    return response.status(400).json({ error: "Customer already exists" })
+  }
+
+  customers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: []
+  });
+
+  return response.status(201).send();
+});
+
+//Consultar extrato
+app.get('/statement/:cpf', verifyExistsAccountCPF, (request, response) => {
+  return response.status(200).json(customer.statement);
+});
 
 
 
-app.listen(5000)
+
+
+app.listen(3333, () => {
+  console.log('Server is up!')
+});
