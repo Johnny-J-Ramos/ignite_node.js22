09c7d02fb34d3ff67add9cf8ecfb050309081e22
Johnny Jefferson José Ramos <61524749+Johnny-J-Ramos@users.noreply.github.com>
2022-05-05 22:58:53 -0300

    criando o extrato por data

--- 01_projeto_nodejs/src/index.js
+++ 01_projeto_nodejs/src/index.js
@@ -102,6 +102,18 @@ app.post('/withdraw', verifyExistsAccountCPF, (request, response) => {
   return response.status(200).send()
 })
 
+//Buscar Extrato por database
+app.get("/statement/date", verifyExistsAccountCPF, (request, response)=> {
+  const { customer } = request;
+  const { date }  = request.query;
+
+  const dateFormat = new Date(date + " 00:00");
+  
+  const statement = customer.statement.filter(
+      (statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString());
+
+return response.json(statement);
+});
 
 
 
